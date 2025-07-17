afcaa5e574223ea4c57da259e7e0a3df37fa9044
Majestyk Vaughns <Majestyk.vaughns@gmail.com>
2025-07-16 21:10:11 -0500

    .

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -23,10 +23,10 @@ const isDay = ({ sunrise, sunset }, now) => {
   return sunrise * 1000 < now && now < sunset * 1000;
 };
 
-const getWeatherType = (temperture) => {
-  if (temperture > 86) {
+const getWeatherType = (temperature) => {
+  if (temperature > 86) {
     return "hot";
-  } else if (temperture >= 66 && temperture < 86) {
+  } else if (temperature >= 66 && temperature <= 86) {
     return "warm";
   } else {
     return "cold";
